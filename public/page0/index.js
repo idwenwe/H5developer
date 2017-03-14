13873c9a0cce0f5abfb060cfcd7034b752bd6f23
Arvin Huang <1095369685@qq.com>
2017-03-14 17:54:03 +0800

    写成了例子

---
+++ public/page0/index.js
@@ -0,0 +1,26 @@
+concat.dependencyPreload = [
+    {   
+        type:'image',
+        src: ["../public/page1/image/0_1.png",
+        "../public/page1/image/0_2.png",
+        "../public/page1/image/0_3.png",
+        "../public/page1/image/0_4.png",
+        "../public/page1/image/0_5.png",
+        "../public/page1/image/0_6.png",
+        "../public/page1/image/0_7.png",
+        "../public/page1/image/0_8.png",
+        "../public/page1/image/0_9.png",
+        "../public/page1/image/0_71.png",
+        "../public/page1/image/0_72.png"]
+    },
+    
+    {
+        type:'audio',
+        src: ['../public/page1/audio/page0.mp3']
+    
+    }
+];
+
+concat.initalize();
+
+concat.exchangeLoadingPage();
